ec8be6f577f1f6b81c19d2074c56d583b5fe8800
DPirer <pedrillotarzan@gmail.com>
2020-03-19 11:18:16 +0100

    Version 2: El programa se bloquea al final

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
 	
 	// Definimos las varaibles
-	int numero1, numero2, suma = 0, resta = 0, multiplicacion = 0, division = 0;
+	int numero1, numero2, suma = 0, resta = 0, multiplicacion = 0, division = 0, enter = 0;
 	
 	// Pedimos al usuario que introduzca los dos números
 	cout<<"Introduzca el primer numero: ";
@@ -21,10 +21,13 @@ int main(){
 	division = numero1 / numero2;
 	
 	// Mostramos los resultados
-	cout<<"La suma es: "<<suma<<endl;
+	cout<<"\nLa suma es: "<<suma<<endl;
 	cout<<"La resta es: "<<resta<<endl;
 	cout<<"La multiplicacion es: "<<multiplicacion<<endl;
-	cout<<"La division es: "<<division<<endl;
+	cout<<"La division es: "<<division<<endl<<endl;
+	
+	// Bloqueamos el programa par que el usuario pueda ver los resultados
+	system("pause");
 	
 	return 0;
 }
